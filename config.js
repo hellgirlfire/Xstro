15df23bac98fd43f390b92432e6b20460a60753f
hellgirlfire <guruminecraft2000@gmail.com>
2025-01-17 04:19:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const config = {
 	SUDO: process.env.SUDO || '',
 	API_ID: process.env.API_ID || 'https://xstro-api-4fb28ece11a9.herokuapp.com',
 	BOT_INFO: process.env.BOT_INFO || 'αѕтяσχ11;χѕтяσ м∂',
-	STICKER_PACK: process.env.STICKER_PACK || 'мα∂є ву;χѕтяσ мυℓтι ∂єνι¢є вσт',
+	STICKER_PACK: process.env.STICKER_PACK || 'Bright Virgo ❤️❤️',
 	WARN_COUNT: process.env.WARN_COUNT || 3,
 	TIME_ZONE: process.env.TIME_ZONE || 'Africa/Lagos',
 	DEBUG: process.env.DEBUG || false,
